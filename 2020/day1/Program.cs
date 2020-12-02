e71d1e1987ee00de125076ff762bdca36a48e8ad
Chris Decker <decker.chris32@gmail.com>
2020-12-02 14:08:34 -0500

    Initial Commit plus day 1 Got day 1

---
+++ 2020/day1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			List<string> myList = new List<string>(System.IO.File.ReadAllLines("numbers.txt"));
+        	foreach(string row in myList) {
+				var number = int.Parse(row);
+				if (number < 2020) {
+					var compliment = 2020 - number;
+					if (myList.Contains(compliment.ToString())) {
+						Console.WriteLine(compliment * number);
+					}
+				}
+			}
+        }
+    }
+}
