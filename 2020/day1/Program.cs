9d8e22d2efcb6ceb7fd303dcabd2ab93b6a08ea3
Chris Decker <decker.chris32@gmail.com>
2020-12-02 14:44:36 -0500

    AdventOfCode formatting fix

--- 2020/day1/Program.cs
+++ 2020/day1/Program.cs
@@ -1,27 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace day1
-{
-	class Program
-	{
-		static void Main(string[] args)
-		{
+namespace day1 {
+	class Program {
+		static void Main(string[] args) {
 
 			List<string> myList = new List<string>(System.IO.File.ReadAllLines("numbers.txt"));
-			foreach (string row in myList)
-			{
+
+			foreach (string row in myList) {
+
 				var number = int.Parse(row);
-				if (number < 2020)
-				{
-					var compliment = 2020 - number;
-					if (myList.Contains(compliment.ToString()))
-					{
-						Console.WriteLine(compliment * number);
+
+				if (number < 2020) {
+
+					if (myList.Contains((2020 - number).ToString())) {
+
+						Console.WriteLine((2020 - number) * number);
+						break;
 					}
 				}
 			}
-			
+
 		}
 	}
 }
