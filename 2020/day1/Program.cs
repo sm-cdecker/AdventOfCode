7496f4fd47d6d70c183ff96249d140c1de41c4c5
Chris Decker <decker.chris32@gmail.com>
2020-12-02 14:10:39 -0500

    AdventOfCode formatting fix TABS DAMMIT

--- 2020/day1/Program.cs
+++ 2020/day1/Program.cs
@@ -3,21 +3,25 @@ using System.Collections.Generic;
 
 namespace day1
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+	class Program
+	{
+		static void Main(string[] args)
+		{
 
 			List<string> myList = new List<string>(System.IO.File.ReadAllLines("numbers.txt"));
-        	foreach(string row in myList) {
+			foreach (string row in myList)
+			{
 				var number = int.Parse(row);
-				if (number < 2020) {
+				if (number < 2020)
+				{
 					var compliment = 2020 - number;
-					if (myList.Contains(compliment.ToString())) {
+					if (myList.Contains(compliment.ToString()))
+					{
 						Console.WriteLine(compliment * number);
 					}
 				}
 			}
-        }
-    }
+			
+		}
+	}
 }
