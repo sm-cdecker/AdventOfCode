25b3be3d7144b92a597b5f49c87b5967407626f2
Chris Decker <decker.chris32@gmail.com>
2020-12-03 14:06:44 -0500

    AdventOfCode 2020 day3 part2

--- 2020/day3/index.js
+++ 2020/day3/index.js
@@ -7,45 +7,73 @@ function main() {
 		.map(row => row.split(''));	
 	
 	console.log(part1(treeGrid));
-	console.log(`wrapLimit: ${treeGrid[0].length}`)
+	
+	console.log(part2(treeGrid));
 }
 
 function part1(treeGrid) {
-	const toboggan = new Toboggan(treeGrid[0].length);
+	const toboggan = new Toboggan(treeGrid[0].length, treeGrid.length);
 
-	return treeGrid.filter((row, idx) => {
-			if (idx === 0)
-				return false;
-			toboggan.move();
-			console.log(toboggan.toString());
-			console.log(row.join(''));
+	let treecount = 0;
+	
+	while(toboggan.move()) {		
+		if (treeGrid[toboggan.row][toboggan.column] === '#') {
+			treecount++;
+		}
+	}
+	return treecount;
+}
 
-			return row[toboggan.column] === '#';
-		}).length;
+function part2(treeGrid) {
+	const toboggans = [
+		new Toboggan(treeGrid[0].length, treeGrid.length, 1, 1),
+		new Toboggan(treeGrid[0].length, treeGrid.length, 1, 3),
+		new Toboggan(treeGrid[0].length, treeGrid.length, 1, 5),
+		new Toboggan(treeGrid[0].length, treeGrid.length, 1, 7),
+		new Toboggan(treeGrid[0].length, treeGrid.length, 2, 1)
+	];
 
+	return toboggans.map(toboggan => {
+		let treecount = 0;
+		
+		while(toboggan.move()) {		
+			if (treeGrid[toboggan.row][toboggan.column] === '#') {
+				treecount++;
+			}
+		}
+		return treecount;
+	}).reduce((prev, curr) => !!prev ? prev * curr : 1);
 }
 
 class Toboggan {
+	row=0;
 	column = 0;
+
+	row_dist=1;
+	column_dist=3;
 	
 	// the numer of columns in the grid, need to know when to wrap
 	wrapLimit = 0;
+	row_limit;
 
-	constructor(wrapLimit) {
+	constructor(wrapLimit, row_limit, row_dist, column_dist) {
 		this.wrapLimit = wrapLimit;
+		this.row_limit = row_limit;
+		this.row_dist = row_dist ? row_dist : this.row_dist;
+		this.column_dist = column_dist ? column_dist : this.column_dist;
 	}
 
 	move() {
-		this.column += 3;
+		this.row += this.row_dist;
+		this.column += this.column_dist;
 		
 		if (this.column >= this.wrapLimit) {
 			this.column -= this.wrapLimit;
 		}
+
+		return this.row < this.row_limit;
 	}
 
-	toString() {
-		return `[${this.column}]`;
-	}
 }
 
 main();
